b12a67e96e558e4dc0cb040a8f3caa316d017a2b
alek08pule <116563708+alek08pule@users.noreply.github.com>
2024-09-08 21:59:15 +0200

    Added initial project files

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import { Routes, Route } from "react-router-dom";
+import Home from "./Pages/Home.js";
+import About from "./Pages/About.js";
+import Projects from "./Pages/Projects.js";
+import Header from "./Components/Header.js";
+import Contact from "./Pages/Contact.js";
+import BackgroundAnimation from "./Components/BackgroundAnimation.js";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BackgroundAnimation />
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/About" element={<About />} />
+        <Route path="/Projects" element={<Projects />} />
+        <Route path="/Contact" element={<Contact />} />
+      </Routes>
     </div>
   );
 }
