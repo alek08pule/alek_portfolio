25270a6989ca7b95db8f94a0a75147d2d5fec57f
alek08pule <116563708+alek08pule@users.noreply.github.com>
2024-09-13 19:57:35 +0200

    style changes2

--- src/Components/BackgroundAnimation.js
+++ src/Components/BackgroundAnimation.js
@@ -4,7 +4,7 @@ import React, { useEffect } from "react";
 import useWindowSize from "../Hooks/useWindowSize";
 
 const colors = ["#0B0909", "#44444C", "#8C8C8C", "#D6D6D6", "#2B89B2"];
-const numStars = 120;
+const numStars = 100;
 
 const BackgroundAnimation = () => {
   const { height, width } = useWindowSize();
