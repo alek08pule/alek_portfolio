7c30a68ae11de9872d98d9830c9520f89986d83d
alek08pule <116563708+alek08pule@users.noreply.github.com>
2024-12-09 17:06:31 +0100

    Updated site with new changes

--- src/Pages/Projects.js
+++ src/Pages/Projects.js
@@ -1,4 +1,3 @@
-import Taxi_app01 from "../Assets/Taxi_app01.jpg";
 import WalletHomePage from "../Assets/Wallet-homePage.jpg";
 import LandingPageLibrary from "../Assets/FrontPage.jpg";
 import News from "../Assets/News.jpg";
@@ -11,27 +10,6 @@ const Projects = () => {
         <h1 className="title">Projects</h1>
         <div className="experience-details-container">
           <div className="project-containers">
-            <div className="project-details">
-              <div className="imageDiv">
-                <img src={Taxi_app01} alt="Project1" />
-              </div>
-              <div className="project-title">
-                <h2 className="experience-sub-title">Taxi</h2>
-              </div>
-              <div className="btn-container">
-                <button
-                  className="btn btn-color-1 project-btn"
-                  onClick={() =>
-                    window.open(
-                      "https://www.figma.com/proto/Vla3N4EiAOvDsX32aENtKy/taxi?page-id=0%3A1&type=design&node-id=2-23&viewport=458%2C112%2C0.32&t=DqmN3k2Ci1Uf6QFb-1&scaling=scale-down&starting-point-node-id=2%3A2&mode=design",
-                      "_blank"
-                    )
-                  }
-                >
-                  Live Demo
-                </button>
-              </div>
-            </div>
             <div className="project-details">
               <div className="imageDiv">
                 <img src={News} alt="Project2" className="project-img" />
